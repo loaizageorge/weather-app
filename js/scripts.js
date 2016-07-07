97c381261e6444e9a3bb4937acea51c130cd6018
George <loaizageorge@gmail.com>
2016-07-06 22:21:42 -0400

    Add skycons as weather icons

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ $(document).ready(getWeather);
 function getWeather(){
     var latitude = "";
     var longitude = "";
+    
+     var skycons = new Skycons({"color": "pink"});
+skycons.set("icon1", Skycons.RAIN); 
+    skycons.play();
     //var temperature ="";
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
@@ -21,8 +25,12 @@ function getWeather(){
     });
   });
 }
-    
-  
+        $("unit").click(function () {
+       var text = $('#units').text();
+       $(this).html(text == "C" ? "F" : "C");
+            alert("Button was clicked");
+    });
+
     
     
 
@@ -31,6 +39,8 @@ function getWeather(){
 function toFahrenheit(kelvin){
     return Math.round(kelvin*(9/5)-459.60);
 }
+
+
 
 
 //cafcb5b05e8a25d0fd9adbd48883493a OPENWEATHER API
