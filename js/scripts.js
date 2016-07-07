a464479bb7210d4b71fc8026f98f3e401a52a489
George <loaizageorge@gmail.com>
2016-07-06 20:41:46 -0400

    working version before icons added

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(getWeather);
+function getWeather(){
+    var latitude = "";
+    var longitude = "";
+    //var temperature ="";
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+      latitude = position.coords.latitude;
+      longitude = position.coords.longitude;
+      
+    $("#putWeather").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+       
+      $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"+&lon="+longitude+"&appid=cafcb5b05e8a25d0fd9adbd48883493a",
+                function(json){
+            
+          // $("#test").html(JSON.stringify(json));
+           $("#conditions").html(json.weather[0].main);
+           var cool = json.main.temp;
+           $("#temperature").html(toFahrenheit(cool));
+           $("#city").html(json.name);
+    });
+  });
+}
+    
+  
+    
+    
+
+}
+
+function toFahrenheit(kelvin){
+    return Math.round(kelvin*(9/5)-459.60);
+}
+
+
+//cafcb5b05e8a25d0fd9adbd48883493a OPENWEATHER API
